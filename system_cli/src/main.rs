e527a4b2434e15d77bef3af2ad72ced28dd1b41d
vremyavnikuda <andrew.nevsky@yandex.ru>
2025-01-25 23:32:34 +0700

    Remove main function from system_cli, update Cargo.toml files for versioning and dependencies, and add command-line argument parsing with tests for OS information retrieval

--- system_cli/src/main.rs
+++ system_cli/src/main.rs
@@ -1,3 +1,62 @@
+use clap::Parser;
+use log::warn;
+
+#[derive(Parser)]
+#[clap(about, version)]
+struct Options {
+    /// Show all OS information.
+    #[clap(long)]
+    all: bool,
+    /// Show OS type.
+    #[clap(short = 't', long = "type")]
+    system_type: bool,
+    /// Show OS version.
+    #[clap(short = 'v', long)]
+    system_version: bool,
+    /// Show OS bitness.
+    #[clap(short, long)]
+    bitness: bool,
+    /// Show OS arch.
+    #[clap(short = 'A', long = "Arch")]
+    architecture: bool,
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    env_logger::init();
+
+    let options = Options::parse();
+    let info = system_info::get();
+
+    if options.all
+        || !(options.system_type || options.system_version || options.bitness || options.architecture)
+    {
+        if options.system_type || options.system_version || options.bitness || options.architecture {
+            warn!("--all supersedes all other options");
+        }
+
+        println!(
+            "OS information:\nType: {}\nVersion: {}\nBitness: {} \narchitecture:{}",
+            info.system_type(),
+            info.version(),
+            info.bit_depth(),
+            info.architecture().unwrap()
+        );
+    } else {
+        if options.system_type {
+            println!("OS type: {}", info.system_type());
+        }
+
+        if options.system_version {
+            println!("OS version: {}", info.version());
+        }
+
+        if options.bitness {
+            println!("OS bitness: {}", info.bit_depth());
+        }
+
+        if options.architecture {
+            println!("OS architecture: {}", info.architecture().unwrap());
+        }
+    }
 }
