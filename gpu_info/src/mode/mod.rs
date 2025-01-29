5760b117427954def489ceaa28178b5dd4556c26
Andrew <112577337+vremyavnikuda@users.noreply.github.com>
2025-01-29 16:22:54 +0700

    Delete gpu_info directory

--- gpu_info/src/mode/mod.rs
+++
@@ -1,7 +0,0 @@
-//! Core module of the `gpu_info` library.
-//!
-//! Contains implementation of the GPU manager (`GpuManager`) and data models.
-//!
-pub mod gpu;
-pub(crate) mod manager;
-pub(crate) mod vendor;
