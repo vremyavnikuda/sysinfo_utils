d67f2bee19c089a02eadbd53f1e4c865736dca44
vremyavnikuda <andrew.nevsky@yandex.ru>
2025-01-29 16:24:49 +0700

    upd

--- system_cli/src/main.rs
+++ system_cli/src/main.rs
@@ -1,3 +1,4 @@
+//! system_info_lib/system_cl/src/main.rs
 use clap::Parser;
 use log::warn;
 
@@ -20,18 +21,105 @@ struct Options {
     #[clap(short = 'A', long = "Arch")]
     architecture: bool,
 }
+#[derive(Parser)]
+#[clap(about, version)]
+struct GpuOptions {
+    /// Show all GPU information.
+    #[clap(long)]
+    all: bool,
+    /// Show GPU vendor.
+    #[clap(short = 'v', long = "name")]
+    vendor: bool,
+    /// Show GPU temperature.
+    #[clap(short = 't', long = "temperature")]
+    temperature: bool,
+    /// Show GPU utilization.
+    #[clap(short = 'u', long = "utilization")]
+    utilization: bool,
+    /// Show GPU power usage.
+    #[clap(short = 's', long = "power")]
+    power_usage: bool,
+    /// Show GPU clock speed.
+    #[clap(short = 'a', long = "clock")]
+    clock_speed: bool,
+}
 
-
+/// Initializes the logging framework, parses command-line options, and retrieves system information.
+///
+/// This function performs the following tasks:
+/// - Initializes the logger using `env_logger::init()`.
+/// - Parses the command-line arguments using `clap` to determine which pieces of system information
+///   to display.
+/// - Retrieves the system information using the `system_info_lib::get()` function.
+/// - Displays the specified system information, including type, version, bit depth, and architecture.
+///   If the `--all` option is specified, all available information is displayed, overriding other options.
+/// - Logs a warning if `--all` supersedes other options.
 fn main() {
     env_logger::init();
-
+    // system_info_lib
     let options = Options::parse();
     let info = system_info::get();
 
+    // gpu_info
+    let gpu_options = GpuOptions::parse();
+    let mut manager = gpu_info::GpuManager::new();
+    manager.refresh();
+
+    if let Some(gpu_info) = manager.gpus.first() {
+        if gpu_options.all
+            || !(gpu_options.vendor
+                || gpu_options.temperature
+                || gpu_options.utilization
+                || gpu_options.power_usage
+                || gpu_options.clock_speed)
+        {
+            if gpu_options.vendor
+                || gpu_options.temperature
+                || gpu_options.utilization
+                || gpu_options.power_usage
+                || gpu_options.clock_speed
+            {
+                warn!("--all supersedes all other options");
+            }
+
+            println!(
+                "GPU information:\n{:?}\n{}\n{}\n{}\n{}",
+                gpu_info.get_name(),
+                gpu_info.get_temperature(),
+                gpu_info.get_utilization(),
+                gpu_info.get_power_usage(),
+                gpu_info.get_clock_speed()
+            );
+        } else {
+            if gpu_options.vendor {
+                println!("GPU Vendor: {:?}", gpu_info.vendor);
+            }
+            if gpu_options.temperature {
+                println!("GPU Temperature: {}", gpu_info.get_temperature());
+            }
+            if gpu_options.utilization {
+                println!("GPU Utilization: {}", gpu_info.get_utilization());
+            }
+            if gpu_options.power_usage {
+                println!("GPU Power Usage: {}", gpu_info.get_power_usage());
+            }
+            if gpu_options.clock_speed {
+                println!("GPU Clock Speed: {}", gpu_info.get_clock_speed());
+            }
+        }
+    }
+
     if options.all
-        || !(options.system_type || options.system_version || options.bit_depth || options.architecture)
+        || !(options.system_type
+            || options.system_version
+            || options.bit_depth
+            || options.architecture)
     {
-        if options.system_type || options.system_version || options.bit_depth || options.architecture {
+        if options.system_type
+            || options.system_version
+            || options.bit_depth
+            || options.architecture
+        {
             warn!("--all supersedes all other options");
         }
 
